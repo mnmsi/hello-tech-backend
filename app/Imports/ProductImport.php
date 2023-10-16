2d72e90f87a0f2cac8de81dcbbeb50522cebc2ef
MahmudIotaIT <mahmudhasan.iotait@gmail.com>
2023-10-16 20:21:34 +0600

    product upload

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -14,13 +14,14 @@ class ProductImport implements ToModel
     */
     public function model(array $row)
     {
+//        dd($row);
         return new Product([
             'brand_id' => 3,
             'category_id' => 2,
             'name' => $row[6],
             'price' => (integer)$row[8],
-            'video_url' => (integer)$row[5],
-            'product_code' => (integer)$row[7],
+            'video_url' => $row[5],
+            'product_code' => $row[7],
             'image_url' => "https://images.entitysport.com/assets/uploads/2023/07/Kerala.png",
             'description' => $row[11],
         ]);
