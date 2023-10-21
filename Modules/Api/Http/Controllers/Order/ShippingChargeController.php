41297be43548ba871e9211a5a1dd60ee3629f835
MahmudIotaIT <mahmudhasan.iotait@gmail.com>
2023-10-21 14:44:34 +0600

    delivery option

--- Modules/Api/Http/Controllers/Order/ShippingChargeController.php
+++ Modules/Api/Http/Controllers/Order/ShippingChargeController.php
@@ -4,21 +4,30 @@ namespace Modules\Api\Http\Controllers\Order;
 
 use App\Http\Controllers\Controller;
 use App\Models\ShippingCharge;
+use App\Models\System\DeliveryOption;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
+
 class ShippingChargeController extends Controller
 {
-    public function shippingCharges($name=null)
+    public function shippingCharges($name = null)
     {
-        $text = strtolower($name);
-        if($text == 'dhaka metro'){
-            $shipping_charge = ShippingCharge::where('title', '=', 'Inside Dhaka')->where('active', 1)->first();
-        }else{
-            $shipping_charge = ShippingCharge::whereNotIn('title', ['Inside Dhaka'])->where('active', 1)->first();
+        try {
+            $text = strtolower($name);
+            if ($text == 'dhaka metro') {
+                $shipping_charge = DeliveryOption::where('name', '=', 'Inside Dhaka')->first();
+            } else {
+                $shipping_charge = DeliveryOption::whereNotIn('name', ['Inside Dhaka'])->first();
+            }
+            return response()->json([
+                'status' => 'success',
+                'data' => $shipping_charge
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
         }
-        return response()->json([
-            'status' => 'success',
-            'data' => $shipping_charge
-        ]);
     }
 }
