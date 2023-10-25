a9a53e030c39c2feb31ebd2d39f852dc82cc712d
unknown <shohagm.iotait@gmail.com>
2023-10-25 13:24:58 +0600

    product upload

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -2,60 +2,52 @@
 
 namespace App\Imports;
 
+use App\Models\Product\Brand;
+use App\Models\Product\Category;
 use App\Models\Product\Product;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-class ProductImport implements ToModel
+use Maatwebsite\Excel\Concerns\WithStartRow;
+
+class ProductImport implements ToModel, WithStartRow
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     * @throws \ErrorException
+     */
     public function model(array $row)
     {
-//        dd($row);
-//        dd(file_get_contents($row[3]));
-//        $contents = file_get_contents($row[3]);
-//        $name = substr($row[3], strrpos($row[3], '/') + 1);
-//        $fff = Storage::put($name, $contents);
-//        $url = $row[3];
-        $imageUrl = $row[3];
-
-// Download the image content
-
-        if($imageUrl)
-        {
-            $response = Http::get($imageUrl);
-            if ($response->successful()) {
-                $imageData = $response->body();
-                // Generate a unique file name or use the original name
-                $fileName = 'unique_filename.jpg';
-                // Save the image to your desired storage disk
-                Storage::disk('public')->put($fileName, $imageData);
-
-                // Optionally, you can store the file path in your database
-                // Example:
-                $imageUrl = Storage::disk('public')->url($fileName);
-
-                // $imageUrl can be saved in your database or used for display
-            } else {
-                // Handle the case when the HTTP request to the image URL fails
-            }
+        $brand = Brand::where("name", $row[0])->first();
+        $category = Category::where("name", $row[1])->first();
+        if ($brand == null && !empty($row[0])) {
+            throw new \ErrorException('Brand ' . $row[0] . " missing. Please add brand first.");
         }
-
-        return new Product([
-            'brand_id' => 3,
-            'category_id' => 2,
-            'name' => $row[6],
-            'price' => (integer)$row[8],
-            'video_url' => $row[5],
-            'product_code' => $row[7],
-            'image_url' => $imageUrl,
-            'description' => $row[11],
-        ]);
+        if ($category == null && !empty($row[1])) {
+            throw new \ErrorException('Category ' . $row[1] . " missing. Please add category first.");
+        }
+        if (!Product::where("product_code", $row[7])->first() && !empty($row[0])) {
+            return new Product([
+                'brand_id' => $brand->id,
+                'category_id' => $category->id,
+                'name' => $row[6],
+                'price' => (integer)$row[8],
+                'discount_rate' => (integer)$row[9],
+                'is_featured' => Str::upper($row[14]) == "YES" ? 1 : 0,
+                'is_new_arrival' => Str::upper($row[12]) == "YES" ? 1 : 0,
+                'is_active' => 1, // $row[15]
+                'is_official' => Str::upper($row[13]) == "YES" ? 1 : 0,
+                'product_code' => $row[7],
+                'image_url' => "product_image/product_no_image.png",
+                'video_url' => $row[5],
+                'description' => $row[11],
+            ]);
+        }
+        return null;
     }
 
     public function startRow(): int
