36e4de56bba14b52d246e49a8d0bc47cf3c9d6c2
mahmudul121 <shohag12145@gmail.com>
2023-10-29 15:23:26 +0600

    product meta value

--- app/Imports/ProductColorImport.php
+++ app/Imports/ProductColorImport.php
@@ -2,10 +2,14 @@
 
 namespace App\Imports;
 
+use App\Models\Product\Category;
 use App\Models\Product\Product;
 use App\Models\Product\ProductColor;
 use App\Models\ProductFeatureKey;
 use App\Models\ProductFeatureValue;
+use App\Models\ProductMetaKey;
+use App\Models\ProductMetaValue;
+use App\Models\SubCategory;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
 
@@ -51,6 +55,33 @@ class ProductColorImport implements ToModel, WithStartRow
             }
 //            product specification
 //                sub category missing
+            $Subcategory = SubCategory::where('category_id',$product->category_id)->first();
+            if($Subcategory) {
+                $p_meta_key = ProductMetaKey::updateOrCreate([
+                    'category_id' => $Subcategory->category_id,
+                    'sub_category_id' => $Subcategory->id,
+                    'key' => 'Size'
+                ],[
+                    'category_id' => $Subcategory->category_id,
+                    'sub_category_id' => $Subcategory->id,
+                    'key' => 'Size'
+                ]);
+
+                if(!empty($row[17])) {
+                    ProductMetaValue::create([
+                        'product_meta_key_id' => $p_meta_key->id,
+                        'product_id' => $product->id,
+                        'value' => $row[17],
+                    ]);
+                }
+                if(!empty($row[18])) {
+                    ProductMetaValue::create([
+                        'product_meta_key_id' => $p_meta_key->id,
+                        'product_id' => $product->id,
+                        'value' => $row[18],
+                    ]);
+                }
+            }
 //            product color create
                 return new ProductColor([
                     "product_id" => $product->id,
