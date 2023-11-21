90feceaaab0d778b6826f6006fa104094970dcc7
MahmudIotaIT <mahmudhasan.iotait@gmail.com>
2023-11-21 15:54:29 +0600

    price

--- Modules/Api/Http/Controllers/Product/ProductController.php
+++ Modules/Api/Http/Controllers/Product/ProductController.php
@@ -70,9 +70,9 @@ class ProductController extends Controller
             }
         }
         $product = Product::with(['productFeatureValues', 'colors'])->where('id', $request->product_id)->first();
-        $price = $this->calculateDiscountPrice($product->price, $product->discount_rate) + $product->productFeatureValues->whereIn('id', $product_feature_id)->sum('price') + $product->colors->whereIn('id', $request->color_id)->sum('price');
-        $discount_price =  5;
-//        also return discount price after calculation
+        $price = $product->price + $product->productFeatureValues->whereIn('id', $product_feature_id)->sum('price') + $product->colors->whereIn('id', $request->color_id)->sum('price');
+        $discount_price = $this->calculateDiscountPrice($product->price, $product->discount_rate ?? 0) + $product->productFeatureValues->whereIn('id', $product_feature_id)->sum('price') + $product->colors->whereIn('id', $request->color_id)->sum('price');
+        //        also return discount price after calculation
         return $this->respondWithSuccessWithData([
             'price' => $price,
             'discount_price' => $discount_price,
