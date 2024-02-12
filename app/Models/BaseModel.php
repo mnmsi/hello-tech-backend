1bb7407d4b714ff99b10e998b5f7e8c7ef714aa8
saifuliotait <saiful.iotait@gmail.com>
2024-02-12 20:23:38 +0600

    clear dependency cache for address

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Redis;
 
 class BaseModel extends Model
 {
@@ -15,11 +16,28 @@ class BaseModel extends Model
         static::updating(function ($model) {
             // Forget the cache for the updated model
             Cache::forget($model->getTable());
+
+            // clear dependency cache for address
+            $this->clearDependencyCache($model->getTable());
         });
 
         static::deleting(function ($model) {
             // Forget the cache for the deleted model
             Cache::forget($model->getTable());
         });
+    }
+
+    private function clearDependencyCache($table): void
+    {
+        $dependencyAddDB = [
+            'divisions',
+            'cities',
+            'areas',
+        ];
+
+        if (in_array($table, $dependencyAddDB)) {
+            Redis::del('*.cities');
+            Redis::del('*.areas');
+        }
     }
 }
