dea41551282918a1189ede8efad36086c4552486
saifuliotait <saiful.iotait@gmail.com>
2024-02-13 18:34:22 +0600

    address cache

--- Modules/Api/Http/Controllers/System/SystemAddressController.php
+++ Modules/Api/Http/Controllers/System/SystemAddressController.php
@@ -1,73 +1,55 @@
 <?php
 
-    namespace Modules\Api\Http\Controllers\System;
+namespace Modules\Api\Http\Controllers\System;
 
-    use Illuminate\Contracts\Support\Renderable;
-    use Illuminate\Http\Request;
-    use App\Http\Controllers\Controller;
-    use App\Models\System\Division;
-    use Illuminate\Http\JsonResponse;
-    use Illuminate\Support\Facades\Cache;
-    use Modules\Api\Http\Traits\System\SystemTrait;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\System\Division;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Cache;
+use Modules\Api\Http\Traits\System\SystemTrait;
 
-    class SystemAddressController extends Controller
+class SystemAddressController extends Controller
+{
+    use SystemTrait;
+
+    /**
+     * Display a listing of the resource.
+     * @return JsonResponse
+     */
+    public function division()
     {
-        use SystemTrait;
+        $divisions = Cache::rememberForever('divisions', function () {
+            return $this->getDivision();
+        });
 
-        /**
-         * Display a listing of the resource.
-         * @return JsonResponse
-         */
-        public function division()
-        {
-            // Check if the divisions are cached
-            if (Cache::has('divisions')) {
-                return $this->respondWithSuccessWithData(Cache::get('divisions'));
-            }
-
-            $divisions = $this->getDivision();
-
-            // Cache the response forever
-            Cache::forever('divisions', $divisions);
-
-            return $this->respondWithSuccessWithData($divisions);
-        }
-
-        /**
-         * Display a listing of the resource.
-         * @return JsonResponse
-         */
-        public function city($division_id = null)
-        {
-            // Check if the cities are cached
-            if (Cache::has("$division_id.cities")) {
-                return $this->respondWithSuccessWithData(Cache::get("$division_id.cities"));
-            }
-
-            $citiesByDivision = $this->getCityByDivision($division_id);
-
-            // Cache the response forever
-            Cache::forever("$division_id.cities", $citiesByDivision);
-
-            return $this->respondWithSuccessWithData($citiesByDivision);
-        }
-
-        /**
-         * Display a listing of the resource.
-         * @return JsonResponse
-         */
-        public function area($city_id = null)
-        {
-            // Check if the areas are cached
-            if (Cache::has("$city_id.areas")) {
-                return $this->respondWithSuccessWithData(Cache::get("$city_id.areas"));
-            }
-
-            $areasByCity = $this->getAreaByCity($city_id);
-
-            // Cache the response forever
-            Cache::forever("$city_id.areas", $areasByCity);
-
-            return $this->respondWithSuccessWithData($areasByCity);
-        }
+        return $this->respondWithSuccessWithData($divisions);
     }
+
+    /**
+     * Display a listing of the resource.
+     * @return JsonResponse
+     */
+    public function city($division_id = null)
+    {
+        $citiesByDivision = Cache::rememberForever("$division_id.cities", function () use ($division_id) {
+            return $this->getCityByDivision($division_id);
+        });
+
+        return $this->respondWithSuccessWithData($citiesByDivision);
+    }
+
+    /**
+     * Display a listing of the resource.
+     * @return JsonResponse
+     */
+    public function area($city_id = null)
+    {
+        $areasByCity = Cache::rememberForever("$city_id.areas", function () use ($city_id) {
+            return $this->getAreaByCity($city_id);
+        });
+
+        return $this->respondWithSuccessWithData($areasByCity);
+    }
+}
