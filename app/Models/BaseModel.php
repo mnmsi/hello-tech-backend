06b3ab9972c6bc939fd12a12158b2f36e9dc1c14
MahmudIotaIT <mahmudhasan.iotait@gmail.com>
2024-02-14 15:25:22 +0600

    base model

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Product\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
@@ -9,30 +10,41 @@ use Illuminate\Support\Facades\Redis;
 
 class BaseModel extends Model
 {
+    private static $productTables = [
+        'products',
+        'product_colors',
+        'product_data',
+        'product_feature_keys',
+        'product_feature_values',
+        'product_media',
+        'product_meta_keys',
+        'product_meta_values',
+        'product_reviews',
+        'product_specifications',
+    ];
+
     protected static function boot()
     {
         parent::boot();
-        $that = new self();
 
-        static::updating(function ($model) use ($that){
+        static::updating(function ($model)  {
 
-            if ($model->getTable() == 'products') {
-                Cache::forget('products.' . $model->slug);
-            }
-            else {
+            if (in_array($model->getTable(), self::$productTables)) {
+                $product = $model->getProductDetailsById($model->product_id);
+                Cache::forget('products.' . $product->slug);
+            } else {
                 // Forget the cache for the updated model
                 Cache::forget($model->getTable());
 
                 // clear dependency cache for address
-                $that->clearAddressDependencyCache($model->getTable());
+                $model->clearAddressDependencyCache($model->getTable());
             }
         });
 
         static::deleting(function ($model) {
             if ($model->getTable() == 'products') {
                 Cache::forget('products.' . $model->slug);
-            }
-            else {
+            } else {
                 // Forget the cache for the deleted model
                 Cache::forget($model->getTable());
             }
@@ -51,5 +63,10 @@ class BaseModel extends Model
             Redis::del('*.cities');
             Redis::del('*.areas');
         }
+    }
+
+    private function getProductDetailsById($id)
+    {
+        return Product::find($id);
     }
 }
