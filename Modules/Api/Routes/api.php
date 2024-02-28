f8c009f02d5071a7c97828a26b82b6f55d33b63a
MahmudIotaIT <mahmudhasan.iotait@gmail.com>
2024-02-28 10:56:13 +0600

    redirect after update

--- Modules/Api/Routes/api.php
+++ Modules/Api/Routes/api.php
@@ -38,33 +38,32 @@ use Illuminate\Support\Facades\Mail;
 
 // Authenticating Routes
 Route::controller(ApiAuthController::class)->group(function () {
-    Route::match(['get', 'post'], 'login', 'login');
-    Route::post('register', 'register');
-    Route::post('forgot-password', 'forgotPassword');
-    Route::post('reset-password', 'resetPassword');
+    Route::match(['get', 'post'], 'login', 'login'); //------ documented
+    Route::post('register', 'register'); //------ documented
+    Route::post('forgot-password', 'forgotPassword'); //------ documented
+    Route::post('reset-password', 'resetPassword'); //------ documented
     //    social login
-    Route::post('google-login', 'googleLogin');
-    Route::get('logout', 'logout')->middleware('auth:sanctum');
+    Route::post('google-login', 'googleLogin'); //------ documented
+    Route::get('logout', 'logout')->middleware('auth:sanctum'); //------ documented
 });
 
-Route::post('send-otp', [OtpController::class, 'sendOtp'])->name('send-otp'); // Send OTP Routes
-Route::post('verify-otp', [OtpController::class, 'verifyOtp']);               // Verify OTP Routes
+Route::post('send-otp', [OtpController::class, 'sendOtp'])->name('send-otp'); // Send OTP Routes //------ documented
+Route::post('verify-otp', [OtpController::class, 'verifyOtp']);               // Verify OTP Routes //------ documented
 
 // System Routes (Public) or (Guest) Mode
 Route::middleware('guest')->group(function () {
-    Route::get('site-settings', [SiteSettingController::class, 'siteSettings']); // -------------- cached
-    Route::get('seo-settings', [SeoSettingController::class, 'seoSettings']);    // -------------- cached
-    Route::get('about', [AboutController::class, 'index']);                      // -------------- cached
+    Route::get('site-settings', [SiteSettingController::class, 'siteSettings']); // -------------- cached [documented]
+    Route::get('seo-settings', [SeoSettingController::class, 'seoSettings']);    // -------------- cached [documented]
+    Route::get('about', [AboutController::class, 'index']);                      // -------------- cached [documented]
     // Banner Routes
-    Route::get('testimonials', [TestimonialController::class, 'testimonials']);  // Testimonial Routes // -------------- cached
-    Route::get('showrooms', [ShowroomController::class, 'showrooms']);           // Showroom Routes // -------------- cached
-    Route::get('colors', [ColorController::class, 'colors']);                    // Color Routes // -------------- cached
+    Route::get('testimonials', [TestimonialController::class, 'testimonials']);  // Testimonial Routes // -------------- cached [documented]
+//    Route::get('colors', [ColorController::class, 'colors']);                    // Color Routes // -------------- cached [documented]
     //    route for video review
-    Route::get('video-review', [VideoReviewController::class, 'index']);         // -------------- cached
+    Route::get('video-review', [VideoReviewController::class, 'index']);         // -------------- cached [documented]
     // Routes on OrderController
     Route::controller(OrderController::class)->group(function () {
-        Route::get('delivery-options', 'deliveryOptions'); // Delivery Options // -------------- cached
-        Route::get('payment-methods', 'paymentMethods');   // Payment Methods// Shipping Charges // -------------- cached
+        Route::get('delivery-options', 'deliveryOptions'); // Delivery Options // -------------- cached [documented]
+        Route::get('payment-methods', 'paymentMethods');   // Payment Methods// Shipping Charges // -------------- cached [documented]
     });
 });
 
@@ -79,43 +78,41 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::post('update', 'update');
             Route::post('change-password', 'changePassword');
         });;  // User Address Routes
-    });
+    })->middleware('auth:sanctum');
 
     // Routes on address prefix
     Route::controller(UserAddressController::class)->prefix('address')->group(function () {
-        Route::get('/', 'addresses');                // Address List Routes
+        Route::get('/', 'addresses');                // Address List Routes [documented]
         Route::post('store', 'store');
-        Route::get('edit/{id}', 'edit');                            // Address Store Routes
-        Route::post('update/{id}', 'update');                       // Address Update Routes
-        Route::delete('delete/{id}', 'delete');                     // Address Delete Routes
-        Route::get('selected-address/', 'getSelectedAddress');      // Address Delete Routes
+        Route::get('edit/{id}', 'edit');                            // Address Store Routes [documented]
+        Route::post('update/{id}', 'update');                       // Address Update Routes [documented]
+        Route::delete('delete/{id}', 'delete');                     // Address Delete Routes [documented]
+        Route::get('selected-address/', 'getSelectedAddress');      // Address Delete Routes [documented]
     });
 
     //        add review
     Route::controller(ReviewController::class)->group(function () {
-        Route::post('product/add-review', 'store');
+        Route::post('product/add-review', 'store'); // Add Review Routes [documented]
     });
 
     //        Wishlist
     Route::controller(WishlistController::class)->prefix("wishlist")->group(function () {
-        Route::post('add', 'store');
-        Route::get('list', 'list');
-        Route::delete('remove/{id}', 'delete');
+        Route::post('add', 'store'); // Add Wishlist Routes [documented]
+        Route::get('list', 'list'); // Get Wishlist Routes [documented]
+        Route::delete('remove/{id}', 'delete'); // Remove Wishlist Routes [documented]
     });
 
     // Routes on cart prefix
     Route::controller(CartController::class)->prefix('cart')->group(function () {
-        Route::get('/', 'carts');                               // Cart Add/Increase/Decreased Routes
-        Route::post('add', 'store');                            // Get Carted Products
-        Route::delete('remove/{id}', 'removeCart');             // Cart Remove Routes
-        Route::post('update', 'updateCart');                    // Cart Update Routes
-        Route::get('selected-product', 'getSelectedProduct');   // Cart Update Routes
+        Route::get('/', 'carts');                               // Cart Add/Increase/Decreased Routes [documented]
+        Route::post('add', 'store');                            // Get Carted Products Routes [documented]
+        Route::delete('remove/{id}', 'removeCart');             // Cart Remove Routes [documented]
+        Route::post('update', 'updateCart');                    // Cart Update Routes [documented]
+        Route::get('selected-product', 'getSelectedProduct');   // Cart Update Routes [documented]
     });
 
-    Route::post('make-order', [OrderController::class, 'order']);
-    Route::get('order-list', [OrderController::class, 'orderList']);                    // Make Order Routes
-    Route::post('buy-now', [OrderController::class, 'buyNow']);                         // Buy Now Routes
-    Route::post('buy-now/make-order', [OrderController::class, 'makeOrderFromBuyNow']); // Buy Now Routes
+    Route::post('make-order', [OrderController::class, 'order']); // Make Order Routes [documented]
+    Route::get('order-list', [OrderController::class, 'orderList']); // Order List Routes [documented]
 
 });
 
@@ -124,7 +121,7 @@ Route::middleware('guest')->group(function () {
 
     //   product meta
     Route::controller(ProductMetaController::class)->prefix('product-meta')->group(function () {
-        Route::get('category/{slug}', 'productMeta');
+        Route::get('category/{slug}', 'productMeta'); // Product Meta Routes // -------------- [documented]
     });
 
     //    Route on Banner
@@ -144,77 +141,72 @@ Route::middleware('guest')->group(function () {
 
     //Routes on Product Category
     Route::controller(CategoryController::class)->prefix('categories')->group(function () {
-        Route::get('/', 'categories');                         // Product Categories // -------------- cached
-        Route::get('popular-categories', 'popularCategories'); // Product Popular Categories // -------------- cached
-        Route::get('/subcategory', 'subCategories');           // Product Sub Categories // -------------- cached
+        Route::get('/', 'categories');                         // Product Categories // -------------- cached [documented]
+        Route::get('popular-categories', 'popularCategories'); // Product Popular Categories // -------------- cached [documented]
+        Route::get('/subcategory', 'subCategories');           // Product Sub Categories // -------------- cached [documented]
     });
 
     //    Routes on Pre-Order
     Route::controller(PreOrderController::class)->prefix('pre-order')->group(function () {
-        Route::post('/store', 'store');
+        Route::post('/store', 'store');  // Pre-Order Store Routes [documented]
     });
 
     // Routes on product prefix
     Route::prefix('product')->group(function () {
         // Route for product count
         Route::get('counts', [ProductController::class, 'totalProductType']);          // Total Product Count
-        Route::get('review/{id}', [ReviewController::class, 'review']);                // Product Review
-        Route::get('related/{id}', [ProductController::class, 'relatedProduct']);      // Related Product // -------------- cached
+        Route::get('review/{id}', [ReviewController::class, 'review']);                // Product Review [documented]
+        Route::get('related/{id}', [ProductController::class, 'relatedProduct']);      // Related Product // -------------- cached [documented]
     });
 
     //        Route on Terms and Condition
     Route::controller(TermsConditionController::class)->group(function () {
-        Route::get('terms', 'terms');   // -------------- cached
+        Route::get('terms', 'terms');   // -------------- cached [documented]
     });
 
     //        Route on Privacy Policy
     Route::controller(PrivacyPolicyController::class)->group(function () {
-        Route::get('privacy-policy', 'privacyPolicy'); // -------------- cached
+        Route::get('privacy-policy', 'privacyPolicy'); // -------------- cached [documented]
     });
 
-    //        Sell Bike
-    //    Route::controller(SellBikeController::class)->prefix('sell')->group(function () {
-    //        Route::get('bike/{brand_id}', 'bikeByBrand');
-    //    });
 
 
 });
-//Route::get('bike/details/{name}', [BikeController::class, 'details']);
 // Routes on feature prefix
 Route::middleware('product')->group(function () {
     Route::controller(ProductController::class)->prefix('product')->group(function () {
-        Route::get('/featured/{id}', 'getFeaturedProduct');   // Feature product
-        Route::get('/', 'getProduct');                        // Feature product // -------------- cached
-        Route::get('details/{name}', 'details');              // Product Details // -------------- cached
+        Route::get('/featured/{id}', 'getFeaturedProduct');   // Feature product [documented]
+        Route::get('/', 'getProduct');                        // Feature product // -------------- cached [documented]
+        Route::get('details/{name}', 'details');              // Product Details // -------------- cached [documented]
         Route::get('get-data/{id}', 'getProductDataById');
-        Route::post('calculate_product_price', 'calculatePrice');
-        Route::get('related', 'relatedProduct');                                   // Related Product // -------------- cached
-        Route::get('total-review/{id}', [ReviewController::class, 'totalReview']); // Product Review
-        Route::get('get-product-by-brand/{slug}', 'getProductByBrand');            // Product Review
-        Route::get('/new-arrivals', 'newArrivals');                                // Product Review // -------------- cached
-        Route::get('featured-new-arrivals', 'featuredNewArrivals');                // Product Review // -------------- cached
+        Route::post('calculate_product_price', 'calculatePrice');  // Product Price Calculation [documented]
+        Route::get('related', 'relatedProduct');                                   // Related Product // -------------- cached [documented]
+        Route::get('total-review/{id}', [ReviewController::class, 'totalReview']); // Product Review [documented]
+        Route::get('get-product-by-brand/{slug}', 'getProductByBrand');            // Product Review [documented]
+        Route::get('/new-arrivals', 'newArrivals');                                // Product Review // -------------- cached [documented]
+        Route::get('featured-new-arrivals', 'featuredNewArrivals');                // Product Review // -------------- cached [documented]
     });
 
     Route::controller(HomePageSectionController::class)->prefix('dynamic-section')->group(function () {
-        Route::get('/', 'homePageSections');   // feature new arrivals
+        Route::get('/', 'homePageSections');   // feature new arrivals [documented]
     });
 
-    Route::get('promotional-products', [DynamicPageController::class, 'allPromotionalProduct']);
+    Route::get('promotional-products', [DynamicPageController::class, 'allPromotionalProduct']); // Promotional Product // -------------- cached [documented]
 });
 
 // dynamic page api
-Route::get('dynamic-page/{slug}', [DynamicPageController::class, 'allBrandProduct']);
+Route::get('dynamic-page/{slug}', [DynamicPageController::class, 'allBrandProduct']);  // [documented]
 
 //Search Suggestions
-Route::get('search-suggestions/{name}', [ProductController::class, 'searchSuggestions']);
+Route::get('search-suggestions/{name}', [ProductController::class, 'searchSuggestions']); // [documented]
 
 // guest cart
 Route::prefix('guest-cart')->middleware('api-session')->as('guest-cart.')->controller(GuestCartController::class)->group(function () {
-    Route::post('add', 'store')->name('add');
-    Route::get('list/{id}', 'getCartProduct')->name('list');
-    Route::Post('/delete', 'removeProductFromCart')->name('remove');
-    Route::post('update', 'updateCart')->name('update');
-    Route::get('selected-product/{id}', 'getSelectedCartProduct')->name('selected-product');
+    Route::post('add', 'store')->name('add');  // Add Product to Cart [documented]
+    Route::get('list/{id}', 'getCartProduct')->name('list'); // Get Carted Products [documented]
+    Route::Post('/delete', 'removeProductFromCart')->name('remove'); // Remove Product from Cart [documented]
+    Route::post('update', 'updateCart')->name('update'); // Update Carted Products [documented]
+    Route::get('selected-product/{id}', 'getSelectedCartProduct')->name('selected-product'); // Get Selected Carted Products [documented]
 });
 
 Route::prefix('guest-order')->middleware('api-session')->as('guest-order.')->controller(GuestOrderController::class)->group(function () {
@@ -229,8 +221,8 @@ Route::controller(SystemAddressController::class)->group(function () {
 
 Route::get('shipping-charges/{name?}', [ShippingChargeController::class, 'shippingCharges']); //.............. Cached
 
-Route::post('create-guest-user', [GuestCartController::class, 'createGuestUser']);
-Route::get('voucher-discount', [OrderController::class, 'getVoucherDiscount']); // Buy Now Routes
+Route::post('create-guest-user', [GuestCartController::class, 'createGuestUser']); // Create Guest User [documented]
+Route::get('voucher-discount', [OrderController::class, 'getVoucherDiscount']); // Get Voucher Discount
 
 
 //Route::get('order-cart',function (){
